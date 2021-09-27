1c8cd234ef41f9992890769c586e8163217c7698
今井 俊輔 <51112810+mynet-simai@users.noreply.github.com>
2021-09-27 06:53:32 +0000

    不要な処理の削除

--- kickstartar/tests/test_transforms_columns_is_success.py
+++ kickstartar/tests/test_transforms_columns_is_success.py
@@ -1,12 +1,11 @@
-from transforms.columns.is_success import column_int_is_success, column_is_success
+from transforms.columns.is_success import column_is_success
 import unittest
 import pandas as pd
 
 
-class TestIsSuccessBase(unittest.TestCase):
-    def assert_invalid_case(self, test_function: callable):
-        """エラーを出すケースのチェック"""
-
+class TestIsSuccess(unittest.TestCase):
+    def test_invalid_case(self):
+        """エラーを出す場合のテスト"""
         origin = pd.DataFrame({"ID": [1, 2], "status": ["successful", "failed"]})
         cases = {
             "invalid_string_on_status": origin.append(
@@ -25,13 +24,7 @@ class TestIsSuccessBase(unittest.TestCase):
                 with self.assertRaises(
                     ValueError,
                 ):
-                    test_function(case)
-
-
-class TestIsSuccess(TestIsSuccessBase):
-    def test_invalid_case(self):
-        """エラーを出す場合のテスト"""
-        self.assert_invalid_case(column_is_success)
+                    column_is_success(case)
 
     def test_main(self):
         """メインのテスト"""
@@ -40,17 +33,3 @@ class TestIsSuccess(TestIsSuccessBase):
         pd.testing.assert_series_equal(
             actual, pd.Series([True, False], name="is_success")
         )
-
-
-class TestIntIsSuccess(TestIsSuccessBase):
-    def test_invalid_case(self):
-        """エラーを出す場合のテスト"""
-        self.assert_invalid_case(column_int_is_success)
-
-    def test_main(self):
-        """メインのテスト"""
-        actual = column_int_is_success(
-            pd.DataFrame({"status": ["successful", "failed"]})
-        )
-
-        pd.testing.assert_series_equal(actual, pd.Series([1, 0], name="int_is_success"))
--- kickstartar/transforms/columns/is_success.py
+++ kickstartar/transforms/columns/is_success.py
@@ -1,6 +1,5 @@
 import warnings
 import pandas as pd
-from transforms.utils.column_mapping_base import BOOL_MAP, ArgMap, ColumnBase
 
 
 IS_SUCCESS_MAP = {"successful": True, "failed": False}
@@ -29,15 +28,3 @@ def column_is_success(df: pd.DataFrame) -> pd.Series:
         )
 
     return df.status.apply(lambda x: IS_SUCCESS_MAP[x]).rename("is_success")
-
-
-def column_int_is_success(df: pd.DataFrame) -> pd.Series:
-    """成功か否かカラム(int)
-
-    Args:
-        df (pd.DataFrame): 対象の全体のデータセット
-
-    Returns:
-        pd.Series: 成功か否かカラム(1 or 0 のint)
-    """
-    return column_is_success(df).apply(BOOL_MAP.get_int).rename("int_is_success")
