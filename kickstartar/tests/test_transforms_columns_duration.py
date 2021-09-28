214b04e25780dd3046745c3a07132a4db6cc292b
今井 俊輔 <51112810+mynet-simai@users.noreply.github.com>
2021-09-28 09:59:57 +0000

    マイナスの期間でエラーを出すようにした

--- kickstartar/tests/test_transforms_columns_duration.py
+++ kickstartar/tests/test_transforms_columns_duration.py
@@ -21,3 +21,29 @@ class TestDuration(unittest.TestCase):
         expected = pd.Series([19], name="duration")
 
         pd.testing.assert_series_equal(actual, expected)
+
+    def test_invalid(self):
+        """エラーが出るテスト"""
+        origin = pd.DataFrame(
+            {
+                "launched": [
+                    datetime.datetime(2021, 1, 1, 0, 0, 0, tzinfo=UTC),
+                ],
+                "deadline": [datetime.date(2021, 1, 20)],
+            }
+        )
+
+        cases = {
+            "minus_duration": origin.append(
+                {
+                    "launched": datetime.datetime(2021, 1, 1, tzinfo=UTC),
+                    "deadline": datetime.date(2020, 12, 1),
+                },
+                ignore_index=True,
+            )
+        }
+
+        for name, df in cases.items():
+            with self.subTest(case=name):
+                with self.assertRaises(ValueError):
+                    column_duration(df)
--- kickstartar/transforms/columns/duration.py
+++ kickstartar/transforms/columns/duration.py
@@ -19,10 +19,23 @@ def column_duration(df: pd.DataFrame) -> pd.Series:
     if {"launched", "deadline"} - set(df.columns) != set():
         raise ValueError("`launched` and `deadline` columns not found")
 
-    return df.parallel_apply(
+    duration = df.parallel_apply(
         lambda x: (
             pd.to_datetime(x.deadline).replace(tzinfo=x.launched.tzinfo)
             - pd.to_datetime(x.launched)
         ).days,
         axis=1,
     ).rename("duration")
+
+    invalid_duration_pj = df[duration < 0]
+    if len(invalid_duration_pj) != 0:
+        raise ValueError(
+            f"""
+            PJ which `launched` before `deadline` is detected
+            --------------- invalid duration_pj ---------------
+            {invalid_duration_pj}
+            ---------------------------------------------------
+        """
+        )
+
+    return duration
